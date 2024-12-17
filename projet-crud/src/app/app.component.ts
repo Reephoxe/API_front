7000ebb572a1206958ecbd3ac52a1dd90e38d693
sirrazmo <sirrazmo@orange.fr>
2024-12-17 11:59:43 +0100

    Commentaires (ENCORE)

--- projet-crud/src/app/app.component.ts
+++ projet-crud/src/app/app.component.ts
@@ -4,18 +4,18 @@ import { UtilisateurComponent } from './utilisateur/utilisateur/utilisateur.comp
 import { ReservationComponent } from './reservation/reservation/reservation.component';
 
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
+@Component({ // Déclaration de l'application générale, correspond à notre header dans chaque page 
+  selector: 'app-root', // Choix du composant racine 
+  standalone: true, // Rend le composant "autonome"
+  imports: [ //Imports du composant
     UtilisateurComponent,
     ReservationComponent,
     RouterOutlet,
     RouterLink
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  ], 
+  templateUrl: './app.component.html', //
+  styleUrl: './app.component.css'      // Connexion avec les html et css du composant 
 })
 export class AppComponent {
-  title = 'projet-crud';
+  title = 'projet-crud'; //Nom affiché sur l'onglet où est ouverte l'application 
 }
--- projet-crud/src/app/app.config.ts
+++ projet-crud/src/app/app.config.ts
@@ -5,10 +5,11 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import {provideHttpClient, withFetch} from '@angular/common/http';
 
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideClientHydration(),
-    provideHttpClient(withFetch())
+export const appConfig: ApplicationConfig = { // Configuration de l'application 
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), // Détection des évènement dans l'application
+    provideRouter(routes), // Paramétrage des routes (liens de navigation du header)
+    provideClientHydration(), // Ajout des composants intéractifs sur l'application 
+    provideHttpClient(withFetch()) // Récupération du client 
   ]
 };
