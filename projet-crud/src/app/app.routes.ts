b319e8179e261a5419136443878d68a6c423b11c
Reephoxe <113097236+Reephoxe@users.noreply.github.com>
2024-12-17 11:24:11 +0100

    nouvelles routes

--- projet-crud/src/app/app.routes.ts
+++ projet-crud/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { ReservationComponent } from './reservation/reservation/reservation.component';
 import { JeuComponent } from './jeu/jeu/jeu.component';
-import { UtilisateurComponent } from './utilisateur/utilisateur/utilisateur.component';
 import { MapComponent } from './map/map.component';
 import { JeuModifierComponent} from './jeu/jeuModifier/jeuModifier.component';
 
@@ -9,7 +8,6 @@ import { JeuModifierComponent} from './jeu/jeuModifier/jeuModifier.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/jeux', pathMatch: 'full' },
-    { path: 'utilisateurs', component: UtilisateurComponent},
     { path: 'reservations', component: ReservationComponent },
     { path: 'jeux', component: JeuComponent},
     { path: 'map', component: MapComponent},
