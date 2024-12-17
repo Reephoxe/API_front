2e669a5bbca96ae95ded60a1689ffa55df5ba968
sirrazmo <sirrazmo@orange.fr>
2024-12-17 13:54:49 +0100

    Commentaire (Ca doit être le dernier cette fois)

--- projet-crud/src/app/services/JeuService.ts
+++ projet-crud/src/app/services/JeuService.ts
@@ -12,22 +12,22 @@ export class JeuService { // Classe de "manipulation" des données liées aux ai
 
     API_URL: string = "http://localhost:8080/jeux"; // redondant -> peut créer une constante du début du chemin
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { } // Constructeur par défaut 
 
-    getList(): Observable<Jeu[]> {
-        return this.http.get<Jeu[]>(this.API_URL);
+    getList(): Observable<Jeu[]> { // Récupération de l'intégralité de la table jeu
+        return this.http.get<Jeu[]>(this.API_URL); // Retourne sous forme de liste de jeu
     }
-    getById(id : number): Observable<Jeu> {
-        return this.http.get<Jeu>(`${this.API_URL}/${id}`);
+    getById(id : number): Observable<Jeu> { // Récupération d'une aire de jeu selon son identifiant 
+        return this.http.get<Jeu>(`${this.API_URL}/${id}`); // Retourne un objet Jeu
     }
-    createJeu(Jeu: Jeu): Observable<Jeu> {
-        return this.http.post<Jeu>(this.API_URL, Jeu);
+    createJeu(Jeu: Jeu): Observable<Jeu> { // Création d'une nouvelle aire de jeu dans la table jeu
+        return this.http.post<Jeu>(this.API_URL, Jeu); // Retourne l'aire en question 
     }
-    deleteJeu(id : number): Observable<void>{
-        return this.http.delete<void>(`${this.API_URL}/${id}`);
+    deleteJeu(id : number): Observable<void>{ // Supprime une aire de jeu dans la table jeu
+        return this.http.delete<void>(`${this.API_URL}/${id}`); // Ne retourne que la réponse de la requette http dans la console web
     }
-    updadeJeu(id : number, jeu : Jeu): Observable<Jeu>{
-        return this.http.put<Jeu>(`${this.API_URL}/${id}`, jeu);
+    updadeJeu(id : number, jeu : Jeu): Observable<Jeu>{ // Mis à jour totale (put) d'un élément d'une aire de jeu dans la table jeu 
+        return this.http.put<Jeu>(`${this.API_URL}/${id}`, jeu); // Retourne l'aire avec ses nouvelles caractéristiques  
     }
 
 }
--- projet-crud/src/app/services/UtilisateurService.ts
+++ projet-crud/src/app/services/UtilisateurService.ts
@@ -4,23 +4,23 @@ import { Observable } from "rxjs";
 import {Utilisateur} from '../models/utilisateur.model';
 
 
-@Injectable({
-    providedIn: 'root'
+@Injectable({ // Marquage de l'injection de dépendances pour la classe 
+    providedIn: 'root'  // Indiquation de la localisation où les données doivent être récupérées
   })
-export class UtilisateurService {
+export class UtilisateurService { // Classe de "manipulation" des données liées aux utilisateurs
 
-    API_URL : string = "http://localhost:8080/api"; // A changer en fonction du back
+    API_URL : string = "http://localhost:8080/api"; // URL liée au service 
 
-    constructor(private http: HttpClient){}
+    constructor(private http: HttpClient){} // Constructeur par défaut 
 
-	getList(): Observable<Utilisateur[]> {
-		return this.http.get<Utilisateur[]>(this.API_URL);
+	getList(): Observable<Utilisateur[]> { // Récupération de l'intégralité de la table utilisateurs
+		return this.http.get<Utilisateur[]>(this.API_URL); // Retourne sous forme de liste de Utilisateur
     } 
-    getById(id : number): Observable<Utilisateur>{
-        return this.http.get<Utilisateur>('&{this.apiUrl}/${id}');
+    getById(id : number): Observable<Utilisateur>{ // Récupération d'un utilisateur selon son identifiant 
+        return this.http.get<Utilisateur>('&{this.apiUrl}/${id}'); // Retourne un objet utilisateur
     }   
-    createUtilisateur(utilisateur:Utilisateur):Observable<Utilisateur>{
-        return this.http.post<Utilisateur>(this.API_URL, utilisateur);
+    createUtilisateur(utilisateur:Utilisateur):Observable<Utilisateur>{ // Création d'un nouvel utilisateur dans la table utilisateur
+        return this.http.post<Utilisateur>(this.API_URL, utilisateur); // Retourne l'utilisateur en question 
     }
     //Penser à ajouter l'update et delete dans un 2 eme temps
 }
